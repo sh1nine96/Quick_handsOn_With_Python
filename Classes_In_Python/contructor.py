5cced6c6ca3f742e4ee99413c2e4d21d1ed5c195
Shubham Sharma <sh1nine96@gmail.com>
2022-10-09 01:24:27 +0530

    update

--- Classes_In_Python/contructor.py
+++ Classes_In_Python/contructor.py
@@ -10,3 +10,19 @@ benny = Virtual_Pet("black", 8)
 print(rocky.color)
 print(benny.color)
 print(rocky.legs)
+
+
+class Pie:
+    def __init__(self, flavor, ingredients):
+        self.flavor = flavor
+        self.ingredients = ingredients
+
+    def print_ingredients(self):
+        for i in self.ingredients:
+            print(i)
+
+
+applePie = Pie('apple', ['flour', 'eggs', 'apples', 'butter'])
+
+print(applePie.flavor)
+applePie.print_ingredients()
